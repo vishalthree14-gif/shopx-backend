8b4cbb49c5fb167032b2f4af8047d62bb8b832ae
vishalthree14-gif <vishalthree14@gmail.com>
2025-10-27 08:33:42 +0530

    Remove share folder from repo and ignore it

--- share/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/refresh-token', authController.refreshToken);
-
-module.exports = router;
